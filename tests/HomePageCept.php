cc7ce6edc244914013705bb97e2de2f8fff2ee32
Konstantinos Kokkorogiannis <koyandianthos@gmail.com>
2019-06-12 12:59:03 +0300

    Use p4-codeception:build-12 and chrome-debug:3.141.59-oxygen

---
+++ tests/HomePageCept.php
@@ -0,0 +1,11 @@
+<?php
+/*
+$I = new AcceptanceTester($scenario);
+$I->wantTo('check home page has a white header');
+
+$I->amOnPage('/');
+
+$headerBackgroundColor = $I->getCSSValue('nav#header', 'background-color');
+
+$I->assertEquals('rgba(255, 255, 255, 0.9)', $headerBackgroundColor);
+*/
