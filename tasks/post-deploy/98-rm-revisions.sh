0872369a83bb7321eb3242f06f536eff27a70cfb
Stefan D <stefan.duerrenberger@greenpeace.org>
2021-09-16 09:08:34 +0200

    Add a post deploy script to remove revisions from 2016 - 2019 in DEV

---
+++ tasks/post-deploy/98-rm-revisions.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if [ "$APP_ENVIRONMENT" = "development" ]; then
+  wp post delete $(wp post list --post_type='revision' --year=2016 --format=ids ) --force
+  wp post delete $(wp post list --post_type='revision' --year=2017 --format=ids ) --force
+  wp post delete $(wp post list --post_type='revision' --year=2018 --format=ids ) --force
+  wp post delete $(wp post list --post_type='revision' --year=2019 --format=ids ) --force
+fi
+
+
+
+
