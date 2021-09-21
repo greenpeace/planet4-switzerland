0838058eac4a17095c012aebaf6e9f24b3767877
Stefan D <stefan.duerrenberger@greenpeace.org>
2021-09-21 09:17:47 +0200

    Fix revisions script not running in dev

--- tasks/post-deploy/98-rm-revisions.sh
+++ tasks/post-deploy/98-rm-revisions.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-echo "Old revisions check:"
-echo "Environment: $APP_ENVIRONMENT"
-echo "Environment: $APP_ENV"
-
-if [ "$APP_ENVIRONMENT" = "development" ]; then
+if [ "$APP_ENV" = "development" ]; then
+  echo "Old revisions check:"
   wp post delete $(wp post list --post_type='revision' --year=2016 --format=ids ) --force
   wp post delete $(wp post list --post_type='revision' --year=2017 --format=ids ) --force
   wp post delete $(wp post list --post_type='revision' --year=2018 --format=ids ) --force
