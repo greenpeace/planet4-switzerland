f885b82f8da847b9264b89d2e94e91f0298b56b7
Stefan D <stefan.duerrenberger@greenpeace.org>
2021-09-21 07:29:12 +0200

    Test revisions script output

--- tasks/post-deploy/98-rm-revisions.sh
+++ tasks/post-deploy/98-rm-revisions.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+echo "Old revisions check:"
+echo "Environment: $APP_ENVIRONMENT"
+
 if [ "$APP_ENVIRONMENT" = "development" ]; then
   wp post delete $(wp post list --post_type='revision' --year=2016 --format=ids ) --force
   wp post delete $(wp post list --post_type='revision' --year=2017 --format=ids ) --force
