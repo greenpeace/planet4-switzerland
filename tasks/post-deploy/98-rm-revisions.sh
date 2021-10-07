ec7d5270f9b6688aa1bbedf89d3c6cddbf05f6e3
Stefan D <stefan.duerrenberger@greenpeace.org>
2021-10-07 14:41:23 +0200

    Update script to remove revisions from 2016-2018 in all environments

--- tasks/post-deploy/98-rm-revisions.sh
+++ tasks/post-deploy/98-rm-revisions.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
-if [ "$APP_ENV" = "development" ]; then
-  echo "Old revisions check:"
-  wp post delete $(wp post list --post_type='revision' --year=2016 --format=ids ) --force
-  wp post delete $(wp post list --post_type='revision' --year=2017 --format=ids ) --force
-  wp post delete $(wp post list --post_type='revision' --year=2018 --format=ids ) --force
-  wp post delete $(wp post list --post_type='revision' --year=2019 --format=ids ) --force
-fi
-
-
-
-
+wp post delete $(wp post list --post_type='revision' --year=2016 --format=ids ) --force
+wp post delete $(wp post list --post_type='revision' --year=2017 --format=ids ) --force
+wp post delete $(wp post list --post_type='revision' --year=2018 --format=ids ) --force
