18aaf005702156a114804d6385e1608e15388df2
Stefan D <stefan.duerrenberger@greenpeace.org>
2021-10-12 08:03:58 +0200

    Change script to delete revisions from 2019

--- tasks/post-deploy/98-rm-revisions.sh
+++ tasks/post-deploy/98-rm-revisions.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-wp post delete $(wp post list --post_type='revision' --year=2016 --format=ids ) --force
-wp post delete $(wp post list --post_type='revision' --year=2017 --format=ids ) --force
-wp post delete $(wp post list --post_type='revision' --year=2018 --format=ids ) --force
+wp post delete $(wp post list --post_type='revision' --year=2019 --format=ids ) --force
