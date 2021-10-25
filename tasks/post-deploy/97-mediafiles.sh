42a9e8f748afdda27c5dfe211c43987619c9640d
Stefan D <stefan.duerrenberger@greenpeace.org>
2021-10-25 13:19:03 +0200

    Rerun media path script on dev

--- tasks/post-deploy/97-mediafiles.sh
+++ tasks/post-deploy/97-mediafiles.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ "$APP_ENV" = "development" ]; then
-  if [ $(wp option pluck planet4_migrations 2 id) = "P4\MasterTheme\Migrations\M004UpdateMissingMediaPath" ]; then
-    wp option patch update planet4_migrations 2 id "P4\MasterTheme\Migrations\M004UpdateMissingMediaPath-old2"
+  if [ $(wp option pluck planet4_migrations 4 id) = "P4\MasterTheme\Migrations\M004UpdateMissingMediaPath" ]; then
+    wp option patch update planet4_migrations 4 id "P4\MasterTheme\Migrations\M004UpdateMissingMediaPath-old2"
     echo "Run P4_Activator"
     wp p4-run-activator
     wp option get planet4_migrations
