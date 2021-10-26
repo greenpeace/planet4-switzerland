85f49711549a7e5503c07ffba6b46652664128a0
Stefan D <stefan.duerrenberger@greenpeace.org>
2021-10-26 11:38:37 +0200

    Remove revisions from 2020

--- tasks/post-deploy/98-rm-revisions.sh
+++ tasks/post-deploy/98-rm-revisions.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-#wp post delete $(wp post list --post_type='revision' --year=2019 --format=ids ) --force
+wp post delete $(wp post list --post_type='revision' --year=2020 --format=ids ) --force
