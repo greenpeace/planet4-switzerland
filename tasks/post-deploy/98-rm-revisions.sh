b2193e890cfbd3f5ad79436dfb25d5625516e12c
Stefan D <stefan.duerrenberger@greenpeace.org>
2021-10-26 13:57:27 +0200

    Only run revisions deletion in production

--- tasks/post-deploy/98-rm-revisions.sh
+++ tasks/post-deploy/98-rm-revisions.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-
-wp post delete $(wp post list --post_type='revision' --year=2020 --format=ids ) --force
+if [ "$APP_ENV" = "production" ]; then
+  wp post delete $(wp post list --post_type='revision' --year=2020 --format=ids ) --force
+fi 
