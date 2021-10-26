455c1601beb27e2f8220f6bd925d8d728c71904a
Stefan D <stefan.duerrenberger@greenpeace.org>
2021-10-26 15:51:09 +0200

    Disable revision deletion script

--- tasks/post-deploy/98-rm-revisions.sh
+++ tasks/post-deploy/98-rm-revisions.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-if [ "$APP_ENV" = "production" ]; then
-  wp post delete $(wp post list --post_type='revision' --year=2020 --format=ids ) --force
-fi 
+
+#wp post delete $(wp post list --post_type='revision' --year=2020 --format=ids ) --force
