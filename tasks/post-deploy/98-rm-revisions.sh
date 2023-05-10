1de77ae98c4b55aa0c35ec97b0a3e194cd41d125
Stefan D <stefan.duerrenberger@greenpeace.org>
2023-05-10 09:58:36 +0200

    Deactivate revisions script

--- tasks/post-deploy/98-rm-revisions.sh
+++ tasks/post-deploy/98-rm-revisions.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-wp post delete $(wp post list --post_type='revision' --year=2021 --format=ids ) --force
+#wp post delete $(wp post list --post_type='revision' --year=2021 --format=ids ) --force
