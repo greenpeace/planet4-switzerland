713eb668bafe6e68e4866e1dc88dca0d9442cc16
Stefan D <stefan.duerrenberger@greenpeace.org>
2024-07-31 12:26:33 +0200

    Add task to delete all translation strings without translation

--- tasks/post-deploy/99-wpml.sh
+++ tasks/post-deploy/99-wpml.sh
@@ -2,3 +2,7 @@
 
 echo "Set WPML_ST_SYNC_TRANSLATION_FILES..."
 wp config set WPML_ST_SYNC_TRANSLATION_FILES true --raw
+
+# This query deletes all translation strings that don't have a translation
+echo "Clean translation tables"
+wp db query "DELETE FROM wp_icl_strings WHERE id NOT IN (SELECT string_id FROM wp_icl_string_translations);"
