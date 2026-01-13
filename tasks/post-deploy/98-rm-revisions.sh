54864aa91328935d929f535737edf71ddd3ac2da
Stefan Dürrenberger <sd@pm7.ch>
2026-01-13 10:04:59 +0100

    Deactivate commands to remove old revisions

--- tasks/post-deploy/98-rm-revisions.sh
+++ tasks/post-deploy/98-rm-revisions.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-wp post delete $(wp post list --post_type='revision' --year=2019 --format=ids ) --force
-wp post delete $(wp post list --post_type='revision' --year=2020 --format=ids ) --force
-wp post delete $(wp post list --post_type='revision' --year=2021 --format=ids ) --force
-wp post delete $(wp post list --post_type='revision' --year=2022 --format=ids ) --force
-wp post delete $(wp post list --post_type='revision' --year=2023 --format=ids ) --force
+#wp post delete $(wp post list --post_type='revision' --year=2019 --format=ids ) --force
+#wp post delete $(wp post list --post_type='revision' --year=2020 --format=ids ) --force
+#wp post delete $(wp post list --post_type='revision' --year=2021 --format=ids ) --force
+#wp post delete $(wp post list --post_type='revision' --year=2022 --format=ids ) --force
+#wp post delete $(wp post list --post_type='revision' --year=2023 --format=ids ) --force
